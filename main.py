7098d6bd18306ed3807dd00c899931d7d9e234c1
Kacper <luszczkacper42@gmail.com>
2024-11-11 22:26:41 +0100

    added really first version of displaying polynomial

--- main.py
+++ main.py
@@ -1,48 +1,43 @@
 polynomial = "5x^22-6x^33-2x^2-x-x^2-x^2"
 # polynomial = input("Enter a polynomial: ")
-print(polynomial)
 
-powers = []
-resta = []
 def showPolynomial(polynomial):
-    power = ''
-    rest = ''
+    powers = []
+    start = 0
     for i in range(len(polynomial)):
-        if polynomial[i] == '^':
-            i+=1
-            power=''
-            while i < len(polynomial) and polynomial[i].isdigit():
-                power += polynomial[i]
-                i+=1
-            if power:
-                powers.append(power)
-        else:
-            rest = ''
-            while i < len(polynomial) and polynomial[i] != "^":
-                rest += polynomial[i]
-                i+=1
-            if rest:
-                resta.append(rest)
+        if polynomial[i] == "-" or polynomial[i] == "+":
+            powers.append(polynomial[start:i])
+            start = i
+    return powers
 
-showPolynomial(polynomial)
-print(powers)
-print(resta)
-# polynomial = "5x^22-6x^33-2x^2-x-x^2-x^2"
-# print(polynomial)
-#
-# powers = []
-#
-# def showPolynomial(polynomial):
-#     power = ''
-#     for i in range(len(polynomial)):
-#         if polynomial[i] == '^':  # Jeśli napotkamy znak '^', zaczynamy szukać wykładnika
-#             i += 1  # Przechodzimy do następnego znaku, który jest wykładnikiem
-#             power = ''  # Resetujemy zmienną 'power', aby zebrać nowy wykładnik
-#             while i < len(polynomial) and polynomial[i].isdigit():  # Zbieramy cyfry wykładnika
-#                 power += polynomial[i]
-#                 i += 1
-#             if power:  # Jeśli wykładnik został znaleziony, dodajemy go do listy
-#                 powers.append(int(power))
-#
-# showPolynomial(polynomial)
-# print(powers)
+results = showPolynomial(polynomial)
+
+pows = []
+digits = []
+for i in results:
+    digits.append(i.split("^")[0])
+    if len(i.split("^")) == 2:
+        pows.append(i.split("^")[1])
+    else:
+        pows.append(1)
+
+print(pows)
+print(digits)
+
+def toSuper(number):
+    superscript_dict = {
+        '0': '⁰', '1': '¹', '2': '²', '3': '³', '4': '⁴',
+        '5': '⁵', '6': '⁶', '7': '⁷', '8': '⁸', '9': '⁹'
+    }
+
+    # Tworzymy napis z górnymi indeksami dla każdego znaku w wykładniku
+    return ''.join(superscript_dict[digit] for digit in str(number))
+
+lastresult = ''
+for i in range(len(results)):
+    lastresult = lastresult + "{}{}".format(digits[i], toSuper(pows[i]))
+print(lastresult)
+
+#refactor
+#add comments
+#build one function of that functionality
